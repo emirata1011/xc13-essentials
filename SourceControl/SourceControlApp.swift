ae63e60b0e7f025ae808510284c72c8053a77d73
EMİR ATA <emirata07@gmail.com>
2022-03-15 21:08:19 +0300

    Initial Commit

---
+++ SourceControl/SourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlApp.swift
+//  SourceControl
+//
+//  Created by EMİR ATA on 15.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct SourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
